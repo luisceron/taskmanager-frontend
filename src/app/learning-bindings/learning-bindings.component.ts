face10d641c30c75be7124dc492e447ab24d409e
Luis Ceron <luismarcoceron@gmail.com>
2017-11-25 16:37:53 -0200

    logic to front

--- src/app/learning-bindings/learning-bindings.component.ts
+++ src/app/learning-bindings/learning-bindings.component.ts
@@ -6,22 +6,42 @@ import { Component } from "@angular/core";
 })
 
 export class LearningBindingsComponent {
+  public mouseClickCount: number;
+  public mouseOverCount: number;
+  public userName: string;
+  public userEmail: string;
+
+  public constructor() {
+    this.mouseClickCount = 0;
+    this.mouseOverCount = 0;
+    this.userName = "Nome Inicial";
+    this.userEmail = "Email Inicial";
+  }
+
+
+
   // mouse events
   public onClick() {
     console.log("Event onClick triggered.");
+    this.mouseClickCount++;
   }
 
   public onMouseOver() {
     console.log("Event onMouseOver triggered.");
+    this.mouseOverCount++;
   }
 
 
   // key events
-  public onKeyDown() {
+  public onKeyDown(event: any) {
     console.log("Event onKeyDown triggered.");
+    console.log(event);
+    this.userName = event.target.value;
   }
 
-  public onKeyUp() {
+  public onKeyUp(event: any) {
     console.log("Event onKeyUp triggered.");
+    console.log(event);
+    this.userEmail = event.target.value;
   }
 }

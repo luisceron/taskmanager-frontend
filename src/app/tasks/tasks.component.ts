0e14920dd939e170733b71edad7e6874a3460b20
Luis Ceron <luismarcoceron@gmail.com>
2017-11-27 18:55:38 -0200

    Creating task service

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -2,30 +2,19 @@ import { Component, OnInit } from '@angular/core';
 
 import { Task } from './shared/task.model';
 
-const TASKS: Array<Task> = [
-  { id: 1, title: 'Fazer tarefa 1' },
-  { id: 2, title: 'Fazer tarefa 2' },
-  { id: 3, title: 'Fazer tarefa 3' },
-  { id: 4, title: 'Fazer tarefa 4' },
-  { id: 5, title: 'Fazer tarefa 5' },
-  { id: 6, title: 'Fazer tarefa 6' },
-  { id: 7, title: 'Fazer tarefa 7' }
-];
-
 @Component({
   selector: 'tasks',
   templateUrl: './tasks.component.html'
 })
 
 export class TasksComponent implements OnInit {
-  public tasks;
+  public tasks: Array<Task>;
   public selectedTask: Task;
 
   public constructor() {
   }
 
   public ngOnInit() {
-    this.tasks = TASKS;
   }
 
   public onSelect(task: Task): void {

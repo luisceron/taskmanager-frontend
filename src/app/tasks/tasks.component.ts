b45afc67075008f8ca6808f64d44d7b3d2333266
Luis Ceron <luismarcoceron@gmail.com>
2017-11-27 23:08:51 -0200

    Adding TaskService and Dependency Injection

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,20 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Task } from './shared/task.model';
+import { TaskService } from './shared/task.service';
 
 @Component({
   selector: 'tasks',
-  templateUrl: './tasks.component.html'
+  templateUrl: './tasks.component.html',
+  providers: [ TaskService ]
 })
 
 export class TasksComponent implements OnInit {
   public tasks: Array<Task>;
   public selectedTask: Task;
 
-  public constructor() {
-  }
+  public constructor(private taskService: TaskService) { }
 
   public ngOnInit() {
+    this.tasks = this.taskService.getTasks();
   }
 
   public onSelect(task: Task): void {
